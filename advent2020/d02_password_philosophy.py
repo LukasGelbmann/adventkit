8426887b0cca40b2b068ebb65285a05f76f21599
Lukas Gelbmann <me@lukasgelbmann.com>
2020-12-02 15:57:58 +0100

    Solve day 2 of 2020

---
+++ advent2020/d02_password_philosophy.py
@@ -0,0 +1,17 @@
+import parse
+
+
+def solve(puzzle_input):
+    rows = parse.mixed_table(puzzle_input)
+
+    first_count = 0
+    for min_appearances, max_appearances, letter, password in rows:
+        if min_appearances <= password.count(letter) <= max_appearances:
+            first_count += 1
+    print(first_count)
+
+    second_count = 0
+    for low, high, letter, password in rows:
+        if (password[low - 1] == letter) != (password[high - 1] == letter):
+            second_count += 1
+    print(second_count)
