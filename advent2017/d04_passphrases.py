f03d28162eb61d3bd43ce06e337ccd5efbe7b7c1
Lukas Gelbmann <me@lukasgelbmann.com>
2020-12-12 16:13:28 +0100

    Solve day 4 of 2017

---
+++ advent2017/d04_passphrases.py
@@ -0,0 +1,22 @@
+import parse
+
+
+def solve(puzzle_input):
+    phrases = parse.string_rows(puzzle_input)
+
+    duplicate_free_count = 0
+    for phrase in phrases:
+        if all_words_unique(phrase):
+            duplicate_free_count += 1
+    print(duplicate_free_count)
+
+    anagram_free_count = 0
+    for phrase in phrases:
+        tidied_phrase = [''.join(sorted(word)) for word in phrase]
+        if all_words_unique(tidied_phrase):
+            anagram_free_count += 1
+    print(anagram_free_count)
+
+
+def all_words_unique(phrase):
+    return len(set(phrase)) == len(phrase)
