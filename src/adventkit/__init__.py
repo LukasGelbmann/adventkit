0e3387ecd1968f43b4f86aca72046e3b64e4c1f1
Lukas Gelbmann <me@lukasgelbmann.com>
2022-09-11 18:53:32 +0200

    Fix capitalization in package docstring

--- src/adventkit/__init__.py
+++ src/adventkit/__init__.py
@@ -8,7 +8,7 @@ refer to a solver module or the solve() function inside it.
 The solver modules of the same Advent of Code edition are grouped together in
 packages. For example, the package `year2019` contains the solvers for Advent
 of Code 2019. The solver modules have names starting with the character 'd'
-(standing for 'day'), followed by the day of the advent calendar.
+(standing for 'day'), followed by the day of the Advent calendar.
 
 The files containing the puzzle input follow a similar naming scheme. For
 example, the input for day 1 of Advent of Code 2019 should be stored in
